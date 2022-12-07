dd4f351a887e4e3c9a60186adf5f698fb2600ff0
Wael Ali Ghamdi <wael.ali.ghamdi@gmail.com>
2022-12-07 10:06:56 +0300

    AoC2022 Project build, and day1 done

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+import day1.day1
+
+fun main() {
+    println("Hello to Advent of Code - 2022!")
+
+    println("--- Day 1: Calorie Counting ---")
+    day1()
+}
---
+++ src/main/kotlin/day1/day1.kt
@@ -0,0 +1,45 @@
+package day1
+
+import java.io.File
+
+fun day1Part1(input: List<String>) {
+    var mostCalories = 0
+    var calories = 0
+
+    for (calorie in input) {
+        if (calorie.isNotBlank()) {
+            calories += calorie.toInt()
+        } else {
+            if (calories > mostCalories) mostCalories = calories
+            calories = 0
+        }
+    }
+    if (calories > mostCalories) mostCalories = calories
+    println(mostCalories)
+}
+
+fun day1Part2(input: List<String>) {
+    // I am trying a different approach to resolve part2
+    val elves = mutableListOf<List<Int>>()
+    var elf = mutableListOf<Int>()
+    for (calorie in input) {
+        if (calorie.isNotBlank()) {
+            elf.add(calorie.toInt())
+        } else {
+            elves.add(elf)
+            elf = mutableListOf()
+        }
+    }
+    elves.add(elf)
+    val elvesCalories = elves.map { it.sum() }
+    println(elvesCalories.sortedDescending().take(3).sum())
+}
+
+fun day1() {
+    val basePath = "src/main/kotlin/day1"
+    val fileName = "input.txt"
+
+    val input = File(basePath, fileName).readLines()
+    day1Part1(input)
+    day1Part2(input)
+}
